d7e990ec8b543b21c70e3cfbe23a51e0208f02a9
John Harrison <john.harrison@sonoco-trident.com>
2018-09-23 17:26:40 +0100

    Initial commit of code

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,7 +6,12 @@ export declare function init(indigoServerURL: string, port: number, username: st
 export declare function GetDevices(): Promise<ObjectHeader[]>;
 export declare function GetDevice(name: string): Promise<Device>;
 export declare function SetDeviceOnOff(name: string, isOn: boolean): Promise<Device>;
+export declare function SetDevicePropertyValue(name: string, propertyname: string, propertyvalue: string): Promise<Device>;
+export declare function ToggleDeviceOnOff(name: string): Promise<Device>;
+export declare function SetDeviceBrightness(name: string, brightness: number): Promise<Device>;
 export declare function GetVariable(name: string): Promise<Variable>;
+export declare function GetVariableValue(name: string): Promise<string>;
+export declare function SetVariable(name: string, value: string): Promise<Variable>;
 export declare function GetActionGroup(name: string): Promise<ActionGroup>;
 export declare function ExecuteActionGroup(name: string): Promise<ActionGroup>;
 export declare function GetVariables(): Promise<ObjectHeader[]>;
